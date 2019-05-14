7f25d3462c5586080209d55fb07e50f6c8226f38
nikitaramoji <nikita_ramoji@brown.edu>
2019-05-14 13:55:14 +0200

    set up firebase

---
+++ app/src/main/java/hu/ait/lifexp/MainActivity.kt
@@ -0,0 +1,73 @@
+package hu.ait.lifexp
+
+import android.content.Intent
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.UserProfileChangeRequest
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    fun registerClick(v: View) {
+        if(!isFormValid()) {
+            return
+        }
+
+        FirebaseAuth.getInstance().createUserWithEmailAndPassword(
+            etEmail.text.toString(), etPassword.text.toString()
+        ).addOnSuccessListener {
+            val user = it.user
+            user.updateProfile(
+                UserProfileChangeRequest.Builder()
+                    .setDisplayName(userNameFromEmail(user.email!!))
+                    .build()
+            )
+
+            Toast.makeText(this@MainActivity, "REGISTER OK", Toast.LENGTH_LONG).show()
+        }.addOnFailureListener {
+            Toast.makeText(this@MainActivity, "Register failed ${it.message}", Toast.LENGTH_LONG).show()
+        }
+    }
+
+    fun loginClick(v: View) {
+        if (!isFormValid()) {
+            return
+        }
+
+        FirebaseAuth.getInstance().signInWithEmailAndPassword(
+            etEmail.text.toString(), etPassword.text.toString()
+        ).addOnSuccessListener {
+            Toast.makeText(this@MainActivity, "LOGIN OK", Toast.LENGTH_LONG).show()
+
+            startActivity(Intent(this@MainActivity, ForumActivity::class.java))
+        }.addOnFailureListener {
+            Toast.makeText(this@MainActivity, "Login failed ${it.message}", Toast.LENGTH_LONG).show()
+        }
+    }
+
+    private fun isFormValid(): Boolean {
+        return when {
+            etEmail.text.isEmpty() -> {
+                etEmail.error = "This field can not be empty"
+                false
+            }
+            etPassword.text.isEmpty() -> {
+                etPassword.error = "This field can not be empty"
+                false
+            }
+            else -> true
+        }
+    }
+
+    private fun userNameFromEmail(email: String) = email.substringBefore("@")
+}
+
+
